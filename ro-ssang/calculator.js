860dbfe275aa2e47a2c1350e405bb9336d636db4
Rosa <fashionfish@naver.com>
2022-05-05 11:29:02 +0900

    Refactor: 자료형 상수를 Type 클래스에서 가져오도록 수정

--- ro-ssang/calculator.js
+++ ro-ssang/calculator.js
@@ -1,28 +1,30 @@
+import Type from './constants/Type'
+
 export default class Calculator {
   constructor(typeChecker) {
     this.typeChecker = typeChecker
   }
 
   add(num1, num2) {
-    if (!this.typeChecker.checkAllTypes([num1, num2], 'number'))
+    if (!this.typeChecker.checkAllTypes([num1, num2], Type.NUMBER))
       throw new Error('숫자만 입력하셔야 합니다.')
     return num1 + num2
   }
 
   substract(num1, num2) {
-    if (!this.typeChecker.checkAllTypes([num1, num2], 'number'))
+    if (!this.typeChecker.checkAllTypes([num1, num2], Type.NUMBER))
       throw new Error('숫자만 입력하셔야 합니다.')
     return num1 - num2
   }
 
   multiply(num1, num2) {
-    if (!this.typeChecker.checkAllTypes([num1, num2], 'number'))
+    if (!this.typeChecker.checkAllTypes([num1, num2], Type.NUMBER))
       throw new Error('숫자만 입력하셔야 합니다.')
     return num1 * num2
   }
 
   divide(num1, num2) {
-    if (!this.typeChecker.checkAllTypes([num1, num2], 'number'))
+    if (!this.typeChecker.checkAllTypes([num1, num2], Type.NUMBER))
       throw new Error('숫자만 입력하셔야 합니다.')
     if (num2 === 0) throw new Error('0으로 나눌 수 없습니다.')
     return Math.floor(num1 / num2)
