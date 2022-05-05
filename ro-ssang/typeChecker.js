6a6b14dbf4b2695c04920858ec660152e996a653
Rosa <fashionfish@naver.com>
2022-05-05 10:39:56 +0900

    Add: 모든 값의 자료형이 같은지 확인하는 checkAllTypes 메서드 추가

--- ro-ssang/typeChecker.js
+++ ro-ssang/typeChecker.js
@@ -2,4 +2,11 @@ export default class TypeChecker {
   checkType(value, type) {
     return typeof value === type
   }
+
+  checkAllTypes(values, type) {
+    for (let i = 0; i < values.length; i++) {
+      if (typeof values[i] !== type) return false
+    }
+    return true
+  }
 }
