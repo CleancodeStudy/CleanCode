4c8d625fc40bed6b31186978ef7198109b8dc8a3
Rosa <fashionfish@naver.com>
2022-05-05 10:57:18 +0900

    Update: Calculator 클래스에서 checkType을 checkAllTypes로 수정

--- ro-ssang/calculator.js
+++ ro-ssang/calculator.js
@@ -4,37 +4,25 @@ export default class Calculator {
   }
 
   add(num1, num2) {
-    if (
-      !this.typeChecker.checkType(num1, 'number') ||
-      !this.typeChecker.checkType(num2, 'number')
-    )
+    if (!this.typeChecker.checkAllTypes([num1, num2], 'number'))
       throw new Error('숫자만 입력하셔야 합니다.')
     return num1 + num2
   }
 
   substract(num1, num2) {
-    if (
-      !this.typeChecker.checkType(num1, 'number') ||
-      !this.typeChecker.checkType(num2, 'number')
-    )
+    if (!this.typeChecker.checkAllTypes([num1, num2], 'number'))
       throw new Error('숫자만 입력하셔야 합니다.')
     return num1 - num2
   }
 
   multiply(num1, num2) {
-    if (
-      !this.typeChecker.checkType(num1, 'number') ||
-      !this.typeChecker.checkType(num2, 'number')
-    )
+    if (!this.typeChecker.checkAllTypes([num1, num2], 'number'))
       throw new Error('숫자만 입력하셔야 합니다.')
     return num1 * num2
   }
 
   divide(num1, num2) {
-    if (
-      !this.typeChecker.checkType(num1, 'number') ||
-      !this.typeChecker.checkType(num2, 'number')
-    )
+    if (!this.typeChecker.checkAllTypes([num1, num2], 'number'))
       throw new Error('숫자만 입력하셔야 합니다.')
     if (num2 === 0) throw new Error('0으로 나눌 수 없습니다.')
     return Math.floor(num1 / num2)
