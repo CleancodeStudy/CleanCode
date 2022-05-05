2aaf80682659e34122e6cfc2f46fab0bd6b807e1
Rosa <fashionfish@naver.com>
2022-05-05 11:42:17 +0900

    Refactor: 에러 메세지를 ErrorMessage 클래스에서 가져오도록 수정

--- ro-ssang/calculator.js
+++ ro-ssang/calculator.js
@@ -1,3 +1,4 @@
+import ErrorMessage from './constants/ErrorMessage'
 import Type from './constants/Type'
 
 export default class Calculator {
@@ -7,26 +8,26 @@ export default class Calculator {
 
   add(num1, num2) {
     if (!this.typeChecker.checkAllTypes([num1, num2], Type.NUMBER))
-      throw new Error('숫자만 입력하셔야 합니다.')
+      throw new Error(ErrorMessage.MANDATORY_NUMBER)
     return num1 + num2
   }
 
   substract(num1, num2) {
     if (!this.typeChecker.checkAllTypes([num1, num2], Type.NUMBER))
-      throw new Error('숫자만 입력하셔야 합니다.')
+      throw new Error(ErrorMessage.MANDATORY_NUMBER)
     return num1 - num2
   }
 
   multiply(num1, num2) {
     if (!this.typeChecker.checkAllTypes([num1, num2], Type.NUMBER))
-      throw new Error('숫자만 입력하셔야 합니다.')
+      throw new Error(ErrorMessage.MANDATORY_NUMBER)
     return num1 * num2
   }
 
   divide(num1, num2) {
     if (!this.typeChecker.checkAllTypes([num1, num2], Type.NUMBER))
-      throw new Error('숫자만 입력하셔야 합니다.')
-    if (num2 === 0) throw new Error('0으로 나눌 수 없습니다.')
+      throw new Error(ErrorMessage.MANDATORY_NUMBER)
+    if (num2 === 0) throw new Error(ErrorMessage.CAN_NOT_DIVIDE_BY_ZERO)
     return Math.floor(num1 / num2)
   }
 }
