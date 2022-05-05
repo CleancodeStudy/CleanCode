bd5487331793c14b7252fc2b0898066aec580ec7
Rosa <fashionfish@naver.com>
2022-05-05 10:28:07 +0900

    Refactor: 타입 확인 로직을 메서드로 분리

--- ro-ssang/calculator.js
+++ ro-ssang/calculator.js
@@ -1,24 +1,28 @@
 export default class Calculate {
+  checkType(value, type) {
+    return typeof value === type
+  }
+
   add(num1, num2) {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number')
+    if (!this.checkType(num1, 'number') || !this.checkType(num2, 'number'))
       throw new Error('숫자만 입력하셔야 합니다.')
     return num1 + num2
   }
 
   substract(num1, num2) {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number')
+    if (!this.checkType(num1, 'number') || !this.checkType(num2, 'number'))
       throw new Error('숫자만 입력하셔야 합니다.')
     return num1 - num2
   }
 
   multiply(num1, num2) {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number')
+    if (!this.checkType(num1, 'number') || !this.checkType(num2, 'number'))
       throw new Error('숫자만 입력하셔야 합니다.')
     return num1 * num2
   }
 
   divide(num1, num2) {
-    if (typeof num1 !== 'number' || typeof num2 !== 'number')
+    if (!this.checkType(num1, 'number') || !this.checkType(num2, 'number'))
       throw new Error('숫자만 입력하셔야 합니다.')
     if (num2 === 0) throw new Error('0으로 나눌 수 없습니다.')
     return Math.floor(num1 / num2)
