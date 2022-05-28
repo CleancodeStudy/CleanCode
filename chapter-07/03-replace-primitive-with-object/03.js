cfe8b428e5ed9ac2b602814ad9d950321ad295f7
Rosa <fashionfish@naver.com>
2022-05-28 09:00:05 +0900

    Add: test ch7-3

---
+++ chapter-07/03-replace-primitive-with-object/03.js
@@ -0,0 +1,24 @@
+class Order {
+  priority = ''
+
+  constructor(data) {
+    this.priority = data.priority
+  }
+}
+
+const client1 = () => {
+  const orders = [
+    { priority: 'high' },
+    { priority: 'rush' },
+    { priority: 'low' },
+    { priority: 'normal' },
+  ].map((o) => new Order(o))
+
+  const highPriorityCount = orders.filter(
+    (o) => o.priority === 'high' || o.priority === 'rush'
+  ).length
+
+  return highPriorityCount
+}
+
+export { client1 }
---
+++ chapter-07/03-replace-primitive-with-object/03.test.js
@@ -0,0 +1,5 @@
+import { client1 } from './03'
+
+it('client1', () => {
+  expect(client1()).toBe(2)
+})
